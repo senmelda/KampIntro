994593497f59ecb301c6c3cb1d9030285c7d2499
Melda Sen <senmelda13@gmail.com>
2021-01-24 00:04:31 +0300

    23 January Lesson Finish

---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Engin Demiroğ
+            GercekMusteri muster1 = new GercekMusteri();
+            muster1.Id = 1;
+            muster1.MusteriNo = "12345";
+            muster1.Adi = "Engin";
+            muster1.Soyadi = "Demiroğ";
+            muster1.TcNo = "12345678910";
+
+            //kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+
+            //Gerçek Müşteri - Tüzel Müşteri ikisi farklı müşterilerdir.
+            //SOLID
+
+
+            Musteri musteri3 = new GercekMusteri();//new : bellekteki referans no Musteri hem gercekmusterinin hemde tuzelmusterinin referansını tutabiliyor.
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(muster1);
+            customerManager.Ekle(musteri2);
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager İhtiyacKrediManager = new IhtiyacKrediManager();
+            
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,  new FileLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {İhtiyacKrediManager, tasitKrediManager };
+           //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
+
+
+//Interfaceleri birbirinin alternatifi olan ama kod içerikleri farklı olan içerikler için kullanırız.
