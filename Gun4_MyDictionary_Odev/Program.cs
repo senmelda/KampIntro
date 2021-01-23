16ee23b9daee821922d4bcc5aec1801c535c8f27
Melda Sen <senmelda13@gmail.com>
2021-01-23 21:11:33 +0300

    new day new homework

---
+++ Gun4_MyDictionary_Odev/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gun4_MyDictionary_Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> sayilar = new MyDictionary<int>();
+            sayilar.Add(79);
+            Console.WriteLine(sayilar);
+        }
+    }
+}
